0f6daca9cf5bc0f17d7c167e8bdeb9ea241099e5
akorb <akorb@geo-konzept.de>
2015-02-05 12:04:18 +0100

    - Removed optional 'this' keywoards to reduce code clutter … - Removed unnecessary multiple setting of properties - In GameOver mode, you can retype your decision even if you applied empty input first - Changed tabs to spaces

--- SnakeBasic/Helpers/DrawHelper.cs
+++ SnakeBasic/Helpers/DrawHelper.cs
@@ -3,29 +3,29 @@ using System.Drawing;
 
 namespace SnakeBasic.Helpers
 {
-  public static class DrawHelper
-  {
-    /// <summary>
-    /// Draws a specified char to a specified point.
-    /// </summary>
-    /// <param name="position">Specifies the point the char should get drawn to.</param>
-    /// <param name="renderingChar">Specifies the char which should get drawn.</param>
-    public static void Draw(Point position, char renderingChar)
-    {
-      Draw(position.X, position.Y, renderingChar);
-    }
+	public static class DrawHelper
+	{
+		/// <summary>
+		/// Draws a specified char to a specified point.
+		/// </summary>
+		/// <param name="position">Specifies the point the char should get drawn to.</param>
+		/// <param name="renderingChar">Specifies the char which should get drawn.</param>
+		public static void Draw(Point position, char renderingChar)
+		{
+			Draw(position.X, position.Y, renderingChar);
+		}
 
-    /// <summary>
-    /// Draws a specified char to specified coordinates.
-    /// </summary>
-    /// <param name="x">Specifies the x coordinate of the point where the char should get drawn.</param>
-    /// <param name="y">Specifies the y coordinate of the point where the char should get drawn.</param>
-    /// <param name="renderingChar">Specifies the char which should get drawn.</param>
-    public static void Draw(int x, int y, char renderingChar)
-    {
-      Console.SetCursorPosition(x, y);
-      Console.Write(renderingChar);
-      Console.SetCursorPosition(0, 0); // Set back to first line that the scrollbar is always on top
-    }
-  }
+		/// <summary>
+		/// Draws a specified char to specified coordinates.
+		/// </summary>
+		/// <param name="x">Specifies the x coordinate of the point where the char should get drawn.</param>
+		/// <param name="y">Specifies the y coordinate of the point where the char should get drawn.</param>
+		/// <param name="renderingChar">Specifies the char which should get drawn.</param>
+		public static void Draw(int x, int y, char renderingChar)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(renderingChar);
+			Console.SetCursorPosition(0, 0); // Set back to first line that the scrollbar is always on top
+		}
+	}
 }
--- SnakeBasic/Program.cs
+++ SnakeBasic/Program.cs
@@ -10,203 +10,209 @@ using Timer = System.Timers.Timer;
 
 namespace SnakeBasic
 {
-  static class Program
-  {
-    /// <summary>
-    /// Gets or sets all existing walls.
-    /// </summary>
-    public static List<Wall> Walls { get; set; }
+	static class Program
+	{
+		/// <summary>
+		/// Gets or sets all existing walls.
+		/// </summary>
+		public static List<Wall> Walls { get; set; }
 
-    /// <summary>
-    /// Gets or sets the snake.
-    /// </summary>
-    public static Snake ActiveSnake { get; set; }
+		/// <summary>
+		/// Gets or sets the snake.
+		/// </summary>
+		public static Snake ActiveSnake { get; set; }
 
-    /// <summary>
-    /// Gets or sets the current goody.
-    /// </summary>
-    public static Candy ActiveCandy { get; set; }
+		/// <summary>
+		/// Gets or sets the current goody.
+		/// </summary>
+		public static Candy ActiveCandy { get; set; }
 
-    /// <summary>
-    /// Gets all entities.
-    /// </summary>
-    public static List<Entity> Entities
-    {
-      get
-      {
-        List<Entity> ents = new List<Entity>();
-        ents.AddRange(Walls);
-        ents.Add(ActiveSnake);
-        ents.Add(ActiveCandy);
-        return ents;
-      }
-    }
+		/// <summary>
+		/// Gets all entities.
+		/// </summary>
+		public static List<Entity> Entities
+		{
+			get
+			{
+				List<Entity> ents = new List<Entity>();
+				ents.AddRange(Walls);
+				ents.Add(ActiveSnake);
+				ents.Add(ActiveCandy);
+				return ents;
+			}
+		}
 
-    /// <summary>
-    /// Gets the timer which controls the movement interval of the snake.
-    /// </summary>
-    public static Timer SnakeTimer { get; private set; }
+		/// <summary>
+		/// Gets the timer which controls the movement interval of the snake.
+		/// </summary>
+		public static Timer SnakeTimer { get; private set; }
 
-    /// <summary>
-    /// Entry point.
-    /// </summary>
-    static void Main()
-    {
-      InitializeGame();
+		/// <summary>
+		/// Entry point.
+		/// </summary>
+		static void Main()
+		{
+			// Set properties valid in every game mode
+			Console.BufferWidth = Console.WindowWidth = 35;
+			Console.BufferHeight = Console.WindowHeight + 1; // + 1 necessary because without the scroll bar jumps if the snake crosses the edges in the last column
+			Console.SetWindowSize(Console.BufferWidth, Console.BufferHeight); // Set window size to buffer size to avoid scroll bars
+			Console.Title = "SnakeBasic";
 
-      Thread keepConsoleOpen = new Thread(() => Thread.Sleep(Timeout.InfiniteTimeSpan));
-      keepConsoleOpen.Start(); // Keeps the console open
-    }
+			InitializeGame();
 
-    /// <summary>
-    /// Returns the entity at specified point, if found; otherwise: null.
-    /// </summary>
-    /// <param name="position">The point of the entity to retrieve.</param>
-    public static Entity EntityAt(Point position)
-    {
-      return Entities.FirstOrDefault(x => x.Coordinates.Contains(position));
-    }
+			Thread keepConsoleOpen = new Thread(() => Thread.Sleep(Timeout.InfiniteTimeSpan));
+			keepConsoleOpen.Start();
+		}
 
-    /// <summary>
-    /// Returns the entity at specified coordinates, if found; otherwise: null.
-    /// </summary>
-    /// <param name="x">The x-coordinate of the entity to retrieve.</param>
-    /// <param name="y">The y-coordinate of the entity to retrieve.</param>
-    public static Entity EntityAt(int x, int y)
-    {
-      return EntityAt(new Point(x, y));
-    }
+		/// <summary>
+		/// Returns the entity at specified point, if found; otherwise: null.
+		/// </summary>
+		/// <param name="position">The point of the entity to retrieve.</param>
+		public static Entity EntityAt(Point position)
+		{
+			return Entities.FirstOrDefault(x => x.Coordinates.Contains(position));
+		}
 
-    /// <summary>
-    /// Initializes necessary components.
-    /// </summary>
-    static void InitializeGame()
-    {
-      Console.Clear(); // Make sure there is nothing
-      Console.BufferWidth = Console.WindowWidth = 35;
-      Console.BufferHeight = Console.WindowHeight + 1; // + 1 necessary because without the scroll bar jumps if the snake crosses the edges in the last column
-      Console.SetWindowSize(Console.BufferWidth, Console.BufferHeight); // Set window size to buffer size to avoid scroll bars
-      Console.Title = "SnakeBasic";
-      Console.CursorVisible = false;
+		/// <summary>
+		/// Returns the entity at specified coordinates, if found; otherwise: null.
+		/// </summary>
+		/// <param name="x">The x-coordinate of the entity to retrieve.</param>
+		/// <param name="y">The y-coordinate of the entity to retrieve.</param>
+		public static Entity EntityAt(int x, int y)
+		{
+			return EntityAt(new Point(x, y));
+		}
 
-      Walls = new List<Wall>(); // Initialize
-      ActiveSnake = new Snake(new Point(10, 10), 4); // Initialize snake
+		/// <summary>
+		/// Initializes necessary components.
+		/// </summary>
+		static void InitializeGame()
+		{
+			Console.CursorVisible = false; // Ensure the cursor is invisible
+			Console.Clear(); // Ensure there is nothing
 
-      Walls = LevelHelper.LoadWalls(WallFormation.FourCrosses);
+			Walls = new List<Wall>(); // Initialize
+			ActiveSnake = new Snake(new Point(10, 10), 4); // Initialize snake
 
-      ActiveCandy = new Candy();
+			Walls = LevelHelper.LoadWalls(WallFormation.FourCrosses);
 
-      // Clear existing characters from the buffer
-      while (Console.KeyAvailable)
-      {
-        Console.ReadKey(true);
-      }
+			ActiveCandy = new Candy();
 
-      // Initialize timer, subscribe and start
-      SnakeTimer = new Timer(100.0);
-      SnakeTimer.Elapsed += timer_Elapsed;
-      SnakeTimer.AutoReset = false;
-      SnakeTimer.Start();
-    }
+			// Clear existing characters from the buffer
+			while (Console.KeyAvailable)
+			{
+				Console.ReadKey(true);
+			}
 
-    static void timer_Elapsed(object sender, ElapsedEventArgs e)
-    {
-      HandleLastKey();
+			// Initialize timer, subscribe and start
+			SnakeTimer = new Timer(100.0);
+			SnakeTimer.Elapsed += timer_Elapsed;
+			SnakeTimer.AutoReset = false;
+			SnakeTimer.Start();
+		}
 
-      #region Update position of snake
+		static void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			HandleLastKey();
 
-      bool goodyEaten = ActiveSnake.HeadPosition == ActiveCandy.Coordinates[0]; // Checks whether the candy has been eaten
+			#region Update position of snake
 
-      Entity collisionEntity = ActiveSnake.UpdatePosition(goodyEaten);
+			bool goodyEaten = ActiveSnake.HeadPosition == ActiveCandy.Coordinates[0]; // Checks whether the candy has been eaten
 
-      if (collisionEntity is Wall || collisionEntity is Snake) // If snake collided with a wall or itself, the game is over
-      {
-        GameOver();
-        return;
-      }
+			Entity collisionEntity = ActiveSnake.UpdatePosition(goodyEaten);
 
-      #endregion
+			if (collisionEntity is Wall || collisionEntity is Snake) // If snake collided with a wall or itself, the game is over
+			{
+				GameOver();
+				return;
+			}
 
-
-      // If goody eaten, we increase the speed by 1 ms
-      // Minimum interval is 20 ms
-      if (goodyEaten)
-      {
-        ActiveCandy = new Candy();
-        if (SnakeTimer.Interval > 20.0)
-          SnakeTimer.Interval -= 1.0;
-      }
-
-      SnakeTimer.Start();
-    }
+			#endregion
 
 
+			// If goody eaten, we increase the speed by 1 ms
+			// Minimum interval is 20 ms
+			if (goodyEaten)
+			{
+				ActiveCandy = new Candy();
+				if (SnakeTimer.Interval > 20.0)
+					SnakeTimer.Interval -= 1.0;
+			}
 
-    /// <summary>
-    /// Handles the last key if there is one.
-    /// </summary>
-    static void HandleLastKey()
-    {
-      while (Console.KeyAvailable)
-      {
-        ConsoleKeyInfo key = Console.ReadKey(true);
-
-        // Only process key if it is the last one.
-        // We only want to the handle the last clicked key.
-        if (Console.KeyAvailable) continue;
-
-        switch (key.Key)
-        {
-          case ConsoleKey.DownArrow:
-            ActiveSnake.MoveDirection = Direction.Down;
-            break;
-
-          case ConsoleKey.LeftArrow:
-            ActiveSnake.MoveDirection = Direction.Left;
-            break;
-
-          case ConsoleKey.RightArrow:
-            ActiveSnake.MoveDirection = Direction.Right;
-            break;
-
-          case ConsoleKey.UpArrow:
-            ActiveSnake.MoveDirection = Direction.Up;
-            break;
-        }
-      }
-    }
-
-    /// <summary>
-    /// Shows the game over dialog.
-    /// </summary>
-    static void GameOver()
-    {
-      SnakeTimer.Elapsed -= timer_Elapsed; // Necessary, otherwise the event might fire one more time!
-      SnakeTimer.Stop();
-      SnakeTimer.Close();
-      SnakeTimer = new Timer();
-
-      Console.Clear();
-      Console.CursorVisible = true;
-
-      Console.WriteLine();
-      Console.WriteLine("GameOver");
-      Console.WriteLine();
-      Console.WriteLine("Repeat?");
-      Console.Write("[Y,N]: ");
+			SnakeTimer.Start();
+		}
 
 
-      string answer = Console.ReadLine();
 
-      if (answer.Length > 0)
-      {
-        char key = answer.ToUpper()[0];
+		/// <summary>
+		/// Handles the last key if there is one.
+		/// </summary>
+		static void HandleLastKey()
+		{
+			while (Console.KeyAvailable)
+			{
+				ConsoleKeyInfo key = Console.ReadKey(true);
 
-        if (key == 'Y')
-          InitializeGame();
-        else
-          Environment.Exit(0);
-      }
-    }
-  }
+				// Only process key if it is the last one.
+				// We only want to the handle the last clicked key.
+				if (Console.KeyAvailable) continue;
+
+				switch (key.Key)
+				{
+					case ConsoleKey.DownArrow:
+						ActiveSnake.MoveDirection = Direction.Down;
+						break;
+
+					case ConsoleKey.LeftArrow:
+						ActiveSnake.MoveDirection = Direction.Left;
+						break;
+
+					case ConsoleKey.RightArrow:
+						ActiveSnake.MoveDirection = Direction.Right;
+						break;
+
+					case ConsoleKey.UpArrow:
+						ActiveSnake.MoveDirection = Direction.Up;
+						break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Shows the game over dialog.
+		/// </summary>
+		static void GameOver()
+		{
+			SnakeTimer.Elapsed -= timer_Elapsed; // Necessary, otherwise the event might fire one more time!
+			SnakeTimer.Stop();
+			SnakeTimer.Close();
+
+			Console.Clear();
+			Console.CursorVisible = true;
+
+			Console.WriteLine();
+			Console.WriteLine("GameOver");
+			Console.WriteLine();
+			Console.WriteLine("Repeat?");
+			Console.Write("[Y,N]: ");
+
+			string answer;
+
+			do
+			{
+				answer = Console.ReadLine();
+
+				if (answer.Length > 0)
+				{
+					char key = answer.ToUpper()[0];
+
+					if (key == 'Y')
+						InitializeGame();
+					else
+						Environment.Exit(0);
+				}
+			}
+			while (answer.Length == 0);
+		}
+	}
 }
