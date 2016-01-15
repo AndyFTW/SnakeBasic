509d9e9454a410940a424772b3cb16243f227c35
Andreas Korb <Andreas.D.Korb@gmail.com>
2016-01-16 00:42:49 +0100

    Removed comments explaining obvious enumerations

--- SnakeBasic/Helpers/Enumerations.cs
+++ SnakeBasic/Helpers/Enumerations.cs
@@ -2,29 +2,11 @@
 
 namespace SnakeBasic.Helpers
 {
-    /// <summary>
-    /// Specifies a direction.
-    /// </summary>
     public enum Direction
     {
-        /// <summary>
-        /// The direction right.
-        /// </summary>
         Right,
-
-        /// <summary>
-        /// The direction down.
-        /// </summary>
         Down,
-
-        /// <summary>
-        /// The direction left.
-        /// </summary>
         Left,
-
-        /// <summary>
-        /// The direction up.
-        /// </summary>
         Up
     }
 
@@ -34,14 +16,7 @@ namespace SnakeBasic.Helpers
     [Flags]
     public enum WallFormation
     {
-        /// <summary>
-        /// A formation in form of a rectangle.
-        /// </summary>
         Rectangle = 1,
-
-        /// <summary>
-        /// A formation in form of four crosses.
-        /// </summary>
         FourCrosses = 2
     }
 }
